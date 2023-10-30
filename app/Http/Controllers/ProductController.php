581544c118e843c922c3aad729066cfa4e70a232
hanhee <susunnn@naver.com>
2023-10-30 14:51:00 +0900

    게시판 수정/삭제

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -11,16 +11,16 @@ class ProductController extends Controller
 
     public function __construct(product $product)
     {
-        // Laravel 의 IOC(Inversion of Control) 입니다
-        // 일단은 이렇게 모델을 가져오는 것이 추천 코드라고 생각하시면 됩니다.
+        // Laravel 의 IOC(Inversion of Control) 입니다.
+        // 일단은 이렇게 모델을 가져오는 것이 추천 코드 방식.
         $this->product = $product;
     }
 
+    // 전체 조회
     public function index()
     {
         // products 의 데이터를 최신순으로 페이징을 해서 가져옵니다.
         $products = $this->product->latest()->paginate(10);
-        // products/index.blade 에 $products 를 보내줍니다
         return view('products.index', compact('products'));
     }
 
@@ -29,15 +29,46 @@ class ProductController extends Controller
         return view('products.create');
     }
 
+    // 생성
     public function store(Request $request)
     {
-        // Request 에 대한 유효성 검사입니다, 다양한 종류가 있기에 공식문서를 보시는 걸 추천드립니다.
+        // Request 에 대한 유효성 검사입니다.
         // 유효성에 걸린 에러는 errors 에 담깁니다.
         $request = $request->validate([
             'name' => 'required',
             'content' => 'required'
         ]);
         $this->product->create($request);
+        return redirect()->route('products.index');
+    }
+
+    // 상세 조회
+    public function show(Product $product)
+    {
+        // show 에 경우는 해당 페이지의 모델 값이 파라미터로 넘어옵니다.
+        return view('products.show', compact('product'));
+    }
+
+    public function edit(Product $product)
+    {
+        return view('products.edit', compact('product'));
+    }
+
+    // 수정
+    public function update(Request $request, Product $product)
+    {
+        $request = $request->validate([
+            'name' => 'required',
+            'content' => 'required'
+        ]);
+        $product->update($request);
+        return redirect()->route('products.index', $product);
+    }
+
+    // 삭제
+    public function destroy(Product $product)
+    {
+        $product->delete();
         return redirect()->route('products.index');
     }
 }
